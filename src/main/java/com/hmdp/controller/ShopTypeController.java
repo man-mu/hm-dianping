6777db3dfef22d9cdedac89670e415fcd62fdd35
cxl <1914009676@qq.com>
2026-03-06 17:27:09 +0800

    添加商户列表缓存

--- src/main/java/com/hmdp/controller/ShopTypeController.java
+++ src/main/java/com/hmdp/controller/ShopTypeController.java
@@ -22,13 +22,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/shop-type")
 public class ShopTypeController {
+
     @Resource
     private IShopTypeService typeService;
 
     @GetMapping("list")
     public Result queryTypeList() {
-        List<ShopType> typeList = typeService
-                .query().orderByAsc("sort").list();
-        return Result.ok(typeList);
+         return typeService.listShopTypes();
     }
 }
--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,18 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TYPE_KEY;
 
 /**
  * <p>
@@ -17,4 +25,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result listShopTypes() {
+        //查询缓存
+        String shopTypeListJSON = stringRedisTemplate.opsForValue().get(CACHE_SHOP_TYPE_KEY);
+        //缓存命中，直接返回数据
+        if (shopTypeListJSON != null && shopTypeListJSON.isBlank()) {
+            List<ShopType> shopTypeList = JSONUtil.toList(shopTypeListJSON, ShopType.class);
+            return Result.ok(shopTypeList);
+        }
+        //缓存未命中，查询数据库
+        List<ShopType> shopTypeList = query().orderByAsc("sort").list();
+        //数据库不存在，返回错误
+        if (shopTypeList == null) {
+            return Result.fail("店铺类型不存在");
+        }
+        //数据库存在，写入缓存并返回
+        stringRedisTemplate.opsForValue().set(CACHE_SHOP_TYPE_KEY, JSONUtil.toJsonStr(shopTypeList));
+        return Result.ok(shopTypeList);
+    }
 }
